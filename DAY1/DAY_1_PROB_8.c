38368171da6d29794e727df880b7e946f73a9a0b
Shashank575 <108973706+Shashank575@users.noreply.github.com>
2022-07-13 09:43:49 +0530

    Create DAY_1_PROB_8.c

---
+++ DAY1/DAY_1_PROB_8.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+   char a;
+   printf("Please enter a character :");
+   scanf("%c",&a);
+   printf("ASCII value of character is %d", a);
+    
+    return 0;
+}
